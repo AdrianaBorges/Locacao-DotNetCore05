dac8abc8ad1cc5e0af424659d65ec837abb8b8a2
Adriana Borges <drika.fonseca.borges@gmail.com>
2022-05-12 00:20:09 -0300

    Criação de entidades;

---
+++ AB.Locacao.Domain/Entities/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AB.Locacao.Domain.Entities
+{
+    public class Pessoa
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+    }
+}
